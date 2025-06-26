a57b9a388e5deb1ba75c19d6aed6420699d0bb5e
Kangchanghwan <lgodl1598@naver.com>
2025-06-26 14:16:39 +0900

    feat: pong 끝

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -51,36 +51,77 @@ public class GameManager : NetworkBehaviour
     // 처음 활성화시 게임을 시작하는 처리를 실행
     public override void OnNetworkSpawn()
     {
-  
+        if (IsServer)
+        {
+            SpawnPlayer();
+            SpawnBall();
+        }
+        IsGameActive = true;
+        gameoverPanel.SetActive(false);
+        UpdateScoreTextClientRpc(0, 0);
+        NetworkManager.OnClientConnectedCallback += OnClientDisconnected;
     }
     
     public override void OnNetworkDespawn()
     {
-
+        NetworkManager.OnClientConnectedCallback -= OnClientDisconnected;
     }
 
     // 게임 도중 클라이언트가 나갔을때 실행
     private void OnClientDisconnected(ulong clinetId)
     {
-
+        if (IsGameActive)
+        {
+            ExitGame();
+        }
     }
 
     // 플레이어들을 스폰
     private void SpawnPlayer()
     {
-        
+        if (NetworkManager.ConnectedClientsList.Count != 2)
+        {
+            Debug.LogError("Network clients can only be spawned once.");
+            return;
+        }
+
+        var playerPrefab = NetworkManager.NetworkConfig.PlayerPrefab;
+
+        for (int i = 0; i < 2; i++)
+        {
+            NetworkClient client = NetworkManager.ConnectedClientsList[i];
+            playerNumberClientIdMap[i] = client.ClientId;
+
+            var spawnPosition = spawnPositionTransforms[i].position;
+            var playerColor = playerColors[i];
+            
+            var playerGameObject = Instantiate(playerPrefab, spawnPosition, Quaternion.identity);
+            var playerPaddle = playerGameObject.GetComponent<PlayerPaddle>();
+            playerPaddle.NetworkObject.SpawnAsPlayerObject(client.ClientId);
+            playerPaddle.SpawnToPositionClientRpc(spawnPosition);
+            playerPaddle.SetRendererColorClientRpc(playerColor);
+        }
     }
 
     // 공 생성
     private void SpawnBall()
     {
-
+        var ballGameObject = Instantiate(ballPrefab, Vector2.zero, Quaternion.identity);
+        var ball = ballGameObject.GetComponent<Ball>();
+        ball.NetworkObject.Spawn();
     }
 
     // 점수 추가
     public void AddScore(int playerNumber, int score)
     {
+      playerScores[playerNumber] += score;
+      UpdateScoreTextClientRpc(playerScores[0], playerScores[1]);
 
+      if (playerScores[playerNumber] >= WinScore)
+      {
+          var winnerId = playerNumberClientIdMap[playerNumber];
+          EndGame(winnerId);
+      }
     }
 
     // 점수 텍스트 갱신
@@ -93,19 +134,39 @@ public class GameManager : NetworkBehaviour
     // 서버에서 실행할 게임 종료
     public void EndGame(ulong winnerId)
     {
-
+        if (!IsServer)
+        {
+            return;
+        }
+        
+        var ball =FindObjectOfType<Ball>();
+        ball.NetworkObject.Despawn();
+        
+        EndGameClientRpc(winnerId);
     }
     
     // 클라이언트에서 실행할 게임 종료 처리
     [ClientRpc]
     public void EndGameClientRpc(ulong winnerId)
     {
+        IsGameActive = false;
 
+        if (winnerId == NetworkManager.LocalClientId)
+        {
+            gameoverText.text = "You won!";
+        }
+        else
+        {
+            gameoverText.text = "You lost!";
+        }
+        
+        gameoverPanel.SetActive(true);
     }
     
     // 게임과 네트워크 연결을 종료하고 메뉴 화면으로 돌아감
     public void ExitGame()
     {
-
+        NetworkManager.Singleton.Shutdown();
+        SceneManager.LoadScene("Menu");
     }
 }
